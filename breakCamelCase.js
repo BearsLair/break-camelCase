751125d242c164d006722f592816289819502c18
Patrick Gross <bears_lair@protonmail.com>
2024-04-22 16:20:23 -0700

    tests set up

---
+++ breakCamelCase.js
@@ -0,0 +1,7 @@
+function solution(string) {
+  return "";
+}
+
+console.log(solution("")); // ""
+console.log(solution("camelCasing")); // "camel Casing"
+console.log(solution("camelCasingTest")); // "camel Casing Test"
